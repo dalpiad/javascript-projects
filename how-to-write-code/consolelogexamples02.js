63155fc26682e2217708b635367b0f477942d146
unknown <dalpiad@gmail.com>
2023-08-16 21:28:02 -0500

    added comments and experimented

--- how-to-write-code/consolelogexamples02.js
+++ how-to-write-code/consolelogexamples02.js
@@ -1,3 +1,3 @@
 console.log("Some Programming Languages:");
 
-console.log("Python\nJavaScript\nJava\nC#\nSwift");
+console.log("Python\tJavaScript\nJava\tC#\tSwift"); //added tabbed spaces the lines by changing the \n to \t 
