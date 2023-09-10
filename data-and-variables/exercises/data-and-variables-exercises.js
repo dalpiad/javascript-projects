0fb34f8ea43323b91e927c9c7d236cee0c4c46f9
Doyle Brown <dalpiad@gmail.com>
2023-09-10 18:56:18 -0500

    Conducted the Data and Variables exercises.

--- data-and-variables/exercises/data-and-variables-exercises.js
+++ data-and-variables/exercises/data-and-variables-exercises.js
@@ -1,11 +1,50 @@
 // Declare and assign the variables below
 
+let shuttleName = 'Determination';
+
+let shuttleSpeed = 17500;
+//Number(shuttleSpeed);
+
+let distanceMarsKM = 225000000;
+//Number(distanceMarsKM);
+
+let distanceMoonKM = 384400;
+//Number(distanceMoonKM);
+
+const milesPerKM = 0.621
+//Number(milesPerKM);
+
 // Use console.log to print the 'typeof' each variable. Print one item per line.
+console.log(typeof shuttleName);
+
+console.log(typeof shuttleSpeed);
+
+console.log(typeof distanceMarsKM);
+
+console.log(typeof distanceMoonKM);
+
+console.log(typeof milesPerKM);
 
 // Calculate a space mission below
 
+let distanceMarsMiles = (distanceMarsKM * milesPerKM);
+
+let timeToMarsHours = distanceMarsMiles / shuttleSpeed;
+
+let timeToMarsDays = timeToMarsHours / 24;
+
 // Print the results of the space mission calculations below.
+
+console.log('The Space Shuttle', shuttleName, 'will take', Math.floor(timeToMarsDays), 'days, and', Math.floor(timeToMarsHours % 24), 'hours, and', Math.round((timeToMarsHours % 24) % 60), 'minutes, to travel to Mars.');
 
 // Calculate a trip to the moon below
 
+let distanceMoonMiles = (distanceMoonKM * milesPerKM);
+
+let timeToMoonHours = distanceMoonMiles / shuttleSpeed;
+
+let timeToMoonDays = timeToMoonHours / 24;
+
 // Print the results of the trip to the moon below
+
+console.log('The Space Shuttle', shuttleName, 'will take', Math.floor(timeToMoonHours % 24), 'hours and', Math.round((timeToMoonHours % 24) % 60), 'minutes, to travel to the Moon.');
--- test.js
+++ test.js
@@ -1,3 +1,30 @@
 // Testing the "typeof" operator.
 
-console.log(typeof "number");
+//console.log(typeof "-34");
+
+// Testing the "number" operator.
+
+//console.log(Number('three'));
+
+// Testing the variables and constants.
+
+//const appName = "Get It Done";
+
+// Testing order of operations.
+
+//console.log(16 - 2 * 5 / 3 + 1);
+
+console.log(1 + 5 % 3);
+
+//console.log(2 ** 2 ** 3 * 3);
+
+//console.log(1 + "1");
+
+// Testing the user input example.
+
+const input = require('readline-sync');  //Load the module. pulls in functions that allow input data from user.
+
+let name = input.question("Enter your name: "); //Prompting the user.
+
+console.log(typeof name);
+//console.log("Hello, " + name + "!"); //Greeting the user with their name as they input the data.
