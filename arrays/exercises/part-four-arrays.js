969c85152d7f76d19f021d90a5015b05135e9c8c
Doyle Brown <dalpiad@gmail.com>
2023-09-17 22:37:17 -0500

    Completed the Array exercises.

--- arrays/exercises/part-four-arrays.js
+++ arrays/exercises/part-four-arrays.js
@@ -4,7 +4,18 @@ let holdCabinet2 = ['orange drink', 'nerf toys', 'camera', 42, 'parsnip'];
 //Explore the methods concat, slice, reverse, and sort to determine which ones alter the original array.
 
 //1) Print the result of using concat on the two arrays. Does concat alter the original arrays? Verify this by printing holdCabinet1 after using the method.
+holdCabinet1.concat(holdCabinet2); //No, concat does not alter the array.
+console.log(holdCabinet1);
 
 //2) Print a slice of two elements from each array. Does slice alter the original arrays?
+console.log(holdCabinet1.slice(0,2)); //No, slice does not alter the array.
+console.log(holdCabinet2.slice(0,2));
 
+console.log(holdCabinet1);
+console.log(holdCabinet2);
 //3) reverse the first array, and sort the second. What is the difference between these two methods? Do the methods alter the original arrays?
+holdCabinet1.reverse(); //Reverse simply reverses the order of the values in the array without comparing the values to one another.
+console.log(holdCabinet1);
+
+holdCabinet2.sort() //Sort arranges the value by treating each as a string, and sorting the the order of the first character as organized in the ASCI table.
+console.log(holdCabinet2);
--- test.js
+++ test.js
@@ -47,29 +47,56 @@
 //     }
 // }
 
-const input = require('readline-sync');
+// const input = require('readline-sync');
 
-// instantiate variables 
-let hoursInWeek = 168;
+// // instantiate variables 
+// let hoursInWeek = 168;
 
-let sleepHours = 56;
-let workHours = 40;
-let miscHours = 21;
-let studyHours = 18;
+// let sleepHours = 56;
+// let workHours = 40;
+// let miscHours = 21;
+// let studyHours = 18;
 
-// create variable to reduce clutter
-let necessities = sleepHours + workHours + miscHours + studyHours;
+// // create variable to reduce clutter
+// let necessities = sleepHours + workHours + miscHours + studyHours;
 
-// instantiate hours spent gaming on teh daily, then multiply by 7 to update leisureHours
-let dailyGaming = Number(input.question("How many hours a day would you like to spend playing Baldur's Gate 3? "));
-let leisureHours = dailyGaming * 7;
+// // instantiate hours spent gaming on teh daily, then multiply by 7 to update leisureHours
+// let dailyGaming = Number(input.question("How many hours a day would you like to spend playing Baldur's Gate 3? "));
+// let leisureHours = dailyGaming * 7;
 
-// give user feedback
-console.log("\nGotcha. You want to spend at least " + dailyGaming + " hours a day playing Baldur's Gate. That comes out to " + leisureHours + " hours spent across seven daily gaming sessions.\n");
+// // give user feedback
+// console.log("\nGotcha. You want to spend at least " + dailyGaming + " hours a day playing Baldur's Gate. That comes out to " + leisureHours + " hours spent across seven daily gaming sessions.\n");
 
-// additional feedback based on whether they have exceeded total hours in a week
-if (necessities + leisureHours > hoursInWeek) {
-  console.log("I think you need to reconsider your priorities. There are only " + hoursInWeek + " hours in a week, and with all your other obligations you only have " + (hoursInWeek - necessities) + " hours throughout the week to spend on leisurely activities.");
-} else {
-  console.log("Cool. That sounds like a balanced schedule. You'll have about " + (hoursInWeek -necessities + leisureHours) + " hours leftover to spend how you'd like.");
-}
+// // additional feedback based on whether they have exceeded total hours in a week
+// if (necessities + leisureHours > hoursInWeek) {
+//   console.log("I think you need to reconsider your priorities. There are only " + hoursInWeek + " hours in a week, and with all your other obligations you only have " + (hoursInWeek - necessities) + " hours throughout the week to spend on leisurely activities.");
+// } else {
+//   console.log("Cool. That sounds like a balanced schedule. You'll have about " + (hoursInWeek -necessities + leisureHours) + " hours leftover to spend how you'd like.");
+// }
+
+// let charles = ['coder', 'Tech', 47, 23, 350];
+// charles.sort();
+// console.log(charles);
+
+// console.log(typeof charles[1]);
+
+// str = 'LaunchCode students rock!';
+// let strArray = str.split(' ');
+
+// console.log(strArray);
+
+// let groceryBag = ['bananas', 'apples', 'edamame', 'chips', 'cucumbers', 'milk', 'cheese'];
+// let selectedItems = [];
+
+// selectedItems = groceryBag.slice(2, 5).sort();
+// console.log(selectedItems);
+
+let school = [
+  ["science", "computer", "art"],
+  ["Jones", "Willoughby", "Rhodes"]
+];
+
+school[0].push("dance");
+console.log(school);
+school[1].push("Holmes");
+console.log(school);
