dc6080540eadebad191d783258eb89dfdeae61c9
Doyle Brown <dalpiad@gmail.com>
2023-09-21 20:25:15 -0500

    Studio updated for loops

--- loops/studio/solution.js
+++ loops/studio/solution.js
@@ -2,11 +2,11 @@ const input = require('readline-sync');
 
 // Part A: #1 Populate these arrays
 
-let protein = [];
-let grains = [];
-let veggies = [];
-let beverages = [];
-let desserts = [];
+let protein = ['chicken', 'pork', 'tofu', 'beef', 'fish', 'beans'];
+let grains = ['rice', 'pasta', 'corn', 'potato', 'quinoa', 'crackers'];
+let veggies = ['peas', 'green beans', 'kale', 'edamame', 'broccoli', 'asparagus'];
+let beverages = ['juice', 'milk', 'water', 'soy milk', 'soda', 'tea'];
+let desserts = ['apple', 'banana', 'more kale', 'ice cream', 'chocolate', 'kiwi'];
 
 
 function mealAssembly(protein, grains, veggies, beverages, desserts, numMeals) {
@@ -15,16 +15,21 @@ function mealAssembly(protein, grains, veggies, beverages, desserts, numMeals) {
   
   /// Part A #2: Write a ``for`` loop inside this function
   /// Code your solution for part A #2 below this comment (and above the return statement) ... ///
-
+  for (let i = 0; i < numMeals; i++) {
+    meals[i] = [pantry[0].splice((Math.floor(Math.random()*pantry[0].length)),1),pantry[1].splice((Math.floor(Math.random()*pantry[1].length)),1),pantry[2].splice((Math.floor(Math.random()*pantry[2].length)),1),pantry[3].splice((Math.floor(Math.random()*pantry[3].length)),1),pantry[4].splice((Math.floor(Math.random()*pantry[4].length)),1)];
+  }Math
 
   return meals;
 }
 
 
 function askForNumber() {
-  numMeals = input.question("How many meals would you like to make?");
+  numMeals = input.question("How many meals would you like to make? ");
   
   /// CODE YOUR SOLUTION TO PART B here ///
+  while (numMeals < 1 || numMeals > 6) {
+    numMeals = input.question("How many meals would you like to make? Must be between 1-6: ");
+  }
 
   return numMeals;
 }
@@ -34,6 +39,18 @@ function generatePassword(string1, string2) {
   let code = '';
 
   /// Code your Bonus Mission Solution here ///
+  //string1 = "";
+  //string2 = "";
+  
+  let codeLength = input.question("How many characters would you like you password? Must be an even integer: ")
+  
+  codeArray = [];
+  
+  for (let i = 0; i < (codeLength/2); i++) {
+    codeArray.push(string1[Math.floor(Math.random()*string1.length)] + string2[Math.floor(Math.random()*string2.length)]); 
+  }
+  
+  code = codeArray.join('');
 
   return code;
 }
@@ -45,24 +62,24 @@ function runProgram() {
   /// Change the final input variable (aka numMeals) here to ensure your solution makes the right number of meals ///
   /// We've started with the number 2 for now. Does your solution still work if you change this value? ///
   
-  // let meals = mealAssembly(protein, grains, veggies, beverages, desserts, 2);
-  // console.log(meals)
+  //let meals = mealAssembly(protein, grains, veggies, beverages, desserts, 6);
+  //console.log(meals);
   
 
   /// TEST PART B HERE ///
   /// UNCOMMENT the next two lines to test your ``askForNumber`` solution ///
   /// Tip - don't test this part until you're happy with your solution to part A #2 ///
   
-  // let mealsForX = mealAssembly(protein, grains, veggies, beverages, desserts, askForNumber());
-  // console.log(mealsForX);
+   let mealsForX = mealAssembly(protein, grains, veggies, beverages, desserts, askForNumber());
+   console.log(mealsForX);
 
     /// TEST PART C HERE ///
   /// UNCOMMENT the remaining commented lines and change the password1 and password2 strings to ensure your code is doing its job ///
 
-  // let password1 = '';
-  // let password2 = '';
-  // console.log("Time to run the password generator so we can update the menu tomorrow.")
-  // console.log(`The new password is: ${generatePassword(password1, password2)}`);
+   let password1 = 'AaBbCcDdEeFfGgHhIiJjKkL';
+   let password2 = '1!2@3#4$5%6^7&8*9?0~';
+   console.log("Time to run the password generator so we can update the menu tomorrow.")
+   console.log(`The new password is: ${generatePassword(password1, password2)}`);
 }
 
 module.exports = {
--- test.js
+++ test.js
@@ -1,183 +1,41 @@
 const input = require('readline-sync');  //Load the module. pulls in functions that allow input data from user.
 
-// Testing the "typeof" operator.
-
-//console.log(typeof "-34");
-
-// Testing the "number" operator.
-
-//console.log(Number('three'));
-
-// Testing the variables and constants.
-
-//const appName = "Get It Done";
-
-// Testing order of operations.
-
-//console.log(16 - 2 * 5 / 3 + 1);
-
-//console.log(1 + 5 % 3);
-
-//console.log(2 ** 2 ** 3 * 3);
-
-//console.log(1 + "1");
-
-// Testing the user input example.
-
-//const input = require('readline-sync');  //Load the module. pulls in functions that allow input data from user.
-
-//let name = input.question("Enter your name: "); //Prompting the user.
-
-//console.log(typeof name);
-//console.log("Hello, " + name + "!"); //Greeting the user with their name as they input the data.
-
-//Testing understanding of if, else if, and else conditionals.
-//let a = 7;
-//if (a % 2 === 1) {
-//   console.log("Launch");
-//} else if (a > 5) {
-//   console.log("Code");
-//} else {
-//   console.log("LaunchCode");
-//}
-
-// let num = 5;
-
-// if (num % 2 === 0) {
-//     if (num % 2 === 1) {
-//         console.log("odd");
-//     }
+// for (let i = 0; i < 51; i++) {
+//   console.log(i);
 // }
 
-// const input = require('readline-sync');
+ let code = '';
 
-// // instantiate variables 
-// let hoursInWeek = 168;
+ /// Code your Bonus Mission Solution here ///
+ string1 = "AaBbCcDdEeFfGgHhIiJjKkLl";
+ string2 = "1!2@3#4$5%6^7&8*9?0~";
 
-// let sleepHours = 56;
-// let workHours = 40;
-// let miscHours = 21;
-// let studyHours = 18;
+let codeLength = input.question("How many characters would you like you password? Must be an even integer: ")
+codeArray = [];
 
-// // create variable to reduce clutter
-// let necessities = sleepHours + workHours + miscHours + studyHours;
+for (let i = 0; i < (codeLength/2); i++) {
+ codeArray.push(string1[Math.floor(Math.random()*string1.length)] + string2[Math.floor(Math.random()*string2.length)]); 
+}
 
-// // instantiate hours spent gaming on teh daily, then multiply by 7 to update leisureHours
-// let dailyGaming = Number(input.question("How many hours a day would you like to spend playing Baldur's Gate 3? "));
-// let leisureHours = dailyGaming * 7;
+code = codeArray.join('');
 
-// // give user feedback
-// console.log("\nGotcha. You want to spend at least " + dailyGaming + " hours a day playing Baldur's Gate. That comes out to " + leisureHours + " hours spent across seven daily gaming sessions.\n");
+console.log(code);
 
-// // additional feedback based on whether they have exceeded total hours in a week
-// if (necessities + leisureHours > hoursInWeek) {
-//   console.log("I think you need to reconsider your priorities. There are only " + hoursInWeek + " hours in a week, and with all your other obligations you only have " + (hoursInWeek - necessities) + " hours throughout the week to spend on leisurely activities.");
-// } else {
-//   console.log("Cool. That sounds like a balanced schedule. You'll have about " + (hoursInWeek -necessities + leisureHours) + " hours leftover to spend how you'd like.");
+// let protein = ['chicken', 'pork', 'tofu', 'beef', 'fish', 'beans'];
+// let grains = ['rice', 'pasta', 'corn', 'potato', 'quinoa', 'crackers'];
+// let veggies = ['peas', 'green beans', 'kale', 'edamame', 'broccoli', 'asparagus'];
+// let beverages = ['juice', 'milk', 'water', 'soy milk', 'soda', 'tea'];
+// let desserts = ['apple', 'banana', 'more kale', 'ice cream', 'chocolate', 'kiwi'];
+
+// let pantry = [protein, grains, veggies, beverages, desserts];
+// let meals = [];
+
+// let numMeals = input.question("How many meals would you like to make?");
+
+// /// Part A #2: Write a ``for`` loop inside this function
+// /// Code your solution for part A #2 below this comment (and above the return statement) ... ///
+// for (let i = 0; i < numMeals; i++) {
+//   meals[i] = [pantry[0][i],pantry[1][i],pantry[2][i],pantry[3][i],pantry[4][i]];
 // }
 
-// let charles = ['coder', 'Tech', 47, 23, 350];
-// charles.sort();
-// console.log(charles);
-
-// console.log(typeof charles[1]);
-
-// str = 'LaunchCode students rock!';
-// let strArray = str.split(' ');
-
-// console.log(strArray);
-
-// let groceryBag = ['bananas', 'apples', 'edamame', 'chips', 'cucumbers', 'milk', 'cheese'];
-// let selectedItems = [];
-
-// selectedItems = groceryBag.slice(2, 5).sort();
-// // console.log(selectedItems);
-
-// let school = [
-//   ["science", "computer", "art"],
-//   ["Jones", "Willoughby", "Rhodes"]
-// ];
-
-// school[0].push("dance");
-// console.log(school);
-// school[1].push("Holmes");
-// console.log(school);
-
-// let phrase = "JavaScript rocks!";
-// console.log(phrase.length);
-// console.log(phrase[phrase.length - 8]);
-
-// cityName = "Vienna";
-
-// stateName = "Virginia";
-
-// location = cityName + ", " + stateName;
-
-// console.log(location);
-
-// console.log(location.length);
-
-// let language = "JavaScript";
-// console.log(language.slice(1,6));
-// language.slice(0,5);
-// console.log(language);
-
-// let org = "       The LaunchCode Foundation ";
-// let trimmed = org.trim();
-
-// console.log(trimmed);
-
-// console.log('Launch\nCode');
-
-
-// let pluralNoun = "cows";
-// let name = "Katie";
-// let verb = "flip";
-// let adjective = "nice";
-// let color = "brown";
-
-// console.log(`JavaScript provides a ${color} collection of tools — including ${adjective} syntax and ${pluralNoun}
-// — that allows ${name} to ${verb} with strings.`);
-
-
-// let groceryBag = ['bananas', 'apples', 'edamame', 'chips', 'cucumbers', 'milk', 'cheese'];
-// let selectedItems = [];
-
-// selectedItems = groceryBag.slice(2, 5).sort();
-// console.log(selectedItems);
-
-// const school = [
-//   ["science", "computer", "art"],
-//   ["Jones", "Willoughby", "Rhodes"]
-// ];
-// school[0].push("dance");
-// school[1].splice(0,0,"Holmes");
-// console.log(school);
-
-// const input = require('readline-sync');
-
-// let myString = "Where does the .push() method put things in an array?";
-
-// console.log(myString.indexOf("y"));
-// console.log(myString.length);
-
-// console.log(myString[0]);
-
-// let phrase = "Chili Cook-off";
-
-// for (let i = 0; i < phrase.length - 1; i = i + 3) {
-//    console.log(phrase[i]);
-// }
-
-let questions = ["Who was the first American woman in space? ", "True or false: 5 kilometer == 5000 meters? ", "(5 + 3)/2 * 10 = ? ", "Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2? ", "What is the minimum crew size for the ISS? "];
-let correctAnswers = ["Sally Ride", "true", "40", "Trajectory", "3"];
-let candidateAnswers = [];
-
-  // TODO 1.1b: Ask for candidate's name //
- let candidateName = input.question("What is your name? ");
-
-
-  // TODO 1.2b: Ask candidate the question and assign the response as candidateAnswer //
-  for (let i = 0; i < questions.length; i++) {
-    candidateAnswers[i] = input.question(questions[i]);
-  }
+// console.log(meals);
