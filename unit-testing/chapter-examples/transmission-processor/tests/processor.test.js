1bedd7586c31ceeee7c915b0ab9d45949234007e
Doyle Brown <dalpiad@gmail.com>
2023-10-09 10:17:08 -0500

    unit testing chapter examples

--- unit-testing/chapter-examples/transmission-processor/tests/processor.test.js
+++ unit-testing/chapter-examples/transmission-processor/tests/processor.test.js
@@ -8,20 +8,33 @@ describe("transmission processor", function() {
   });
  
   test("Return '-1' if the string does not contain '::'.", () => {
-    expect(isPalindrome("a")).toBe(true);
+    let result = processor("9701<489584872710>");
+    expect(result).toBe(-1);
   });
 
-  test("Returned object contains an id property.", () => {
-    expect(isPalindrome("a")).toBe(true);
+  test("returns id in object", () => {
+    let result = processor("9701::<489584872710>");
+    expect(result.id).not.toBeUndefined();
   });
 
-  test("Return '-1' if the string does not contain '::'.", () => {
-    expect(isPalindrome("a")).toBe(true);
+  test("converts id to a number", () => {
+    let result = processor("9701::<489584872710>");
+    expect(result.id).toBe(9701);
   });
 
-  test("Return '-1' if the string does not contain '::'.", function() {
-    expect(isPalindrome("a")).toBe(true);
+  test("returns rawData in object", () => {
+    let result = processor("9701::<487297403495720912>");
+    expect(result.rawData).not.toBeUndefined();
   });
 
-
+  test("returns -1 for rawData if missing < at position 0", () => {
+    let result = processor("9701::487297403495720912>");
+    expect(result.rawData).toBe(-1);
  });
+
+ test("returns -1 for rawData if missing > at last position", () => {
+  let result = processor("<9701::487297403495720912");
+  expect(result.rawData).toBe(-1);
+});
+
+});
