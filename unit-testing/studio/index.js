90cfd2f07fd73ad48d2935d97fa1bda69733cbda
Doyle Brown <dalpiad@gmail.com>
2023-10-09 20:22:53 -0500

    completed studio... sorta... mostly....

--- unit-testing/studio/index.js
+++ unit-testing/studio/index.js
@@ -1,10 +1,52 @@
 
+function launchOutput(num) {
+if (num % 2 === 0) {
+    if (num % 3 === 0) {
+        if (num % 5 === 0) {
+            return "LaunchCode Rocks!";
+        }
+        return "LaunchCode!";
+    }
+    if (num % 5 === 0) {
+        return "Launch Rocks! (CRASH!!!!)";
+    }
+    return "Launch!";
+}
+if (num % 3 === 0) {
+    if (num % 5 === 0) {
+        return "Code Rocks!";
+    }
+    return "Code!";
+}
+if (num % 5 === 0) {
+    return "Rocks!";
+}
+
+    /*
+if divisible by 2
+    if divisible 3
+        if divisible by 5
+        return
+    return
+    if divisible by 5
+    return
+if divisible by 3
+    if divisible by 5
+    return
+return
+if divisible by 5
+return
+else return
+*/
+    return "Rutabagas! That doesn't work.";
+}
+
 let launchcode = {
     organizaion: 'nonprofit',
     executiveDirector: 'Jeff',
     percentageCoolEmployees: 100,
     programsOffered: ["Web Development", "Data Analysis", "Liftoff"],
-    launchOutput: launchOutput()  
+    launchOutput: launchOutput
 }
 
 
--- unit-testing/studio/tests/launchcode.test.js
+++ unit-testing/studio/tests/launchcode.test.js
@@ -1,8 +1,43 @@
 // launchcode.test.js code:
 const launchcode = require('../index.js');
 
-describe("Testing launchcode", function(){
+describe("Testing launchcode", () => {
 
   // Write your unit tests here!
+
+  test("Check the launchcode object contains 'nonprofit' under the 'organization' key.", () => {
+    let output = launchcode.organizaion;
+    expect(output).toBe("nonprofit");
+   });
+
+   test("Check the launchcode object contains 'Jeff' under the 'executiveDirector' key.", () => {
+    let output = launchcode.executiveDirector;
+    expect(output).toBe("Jeff");
+   });
+
+   test("Check the launchcode object contains the number 100, under the 'percentageCoolEmployees' key.", () => {
+    let output = launchcode.percentageCoolEmployees;
+    expect(output).toBe(100);
+   });
+
+   test("Checks the content of the array under the 'programsOffered' key", () => {
+    let output = launchcode.programsOffered;
+    expect(launchcode.programsOffered.includes("Web Development")).toBe(true);
+    expect(output.includes("Data Analysis")).toBe(true);
+    expect(output.includes("Liftoff")).toBe(true);
+    expect(output.length).toBe(3);   
+   });
+
+   test("Check the launchOutput returns the correct values based on the input.", () => {
+    let output = launchcode.launchOutput;
+    expect(output(2)).toBe('Launch!');
+    expect(launchcode.launchOutput(3)).toBe('Code!');
+    expect(launchcode.launchOutput(5)).toBe('Rocks!');
+    expect(launchcode.launchOutput(6)).toBe('LaunchCode!');
+    expect(launchcode.launchOutput(15)).toBe('Code Rocks!');
+    expect(launchcode.launchOutput(10)).toBe('Launch Rocks! (CRASH!!!!)');
+    expect(launchcode.launchOutput(30)).toBe('LaunchCode Rocks!');
+    expect(launchcode.launchOutput(29)).toBe("Rutabagas! That doesn't work.");
+   });
   
 });
